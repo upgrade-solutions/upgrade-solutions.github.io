550dd31039ea668192a71b105dffd24b69a4c61d
timkleier <timkleier@gmail.com>
2023-12-03 19:04:08 -0700

    experimental image assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://upgrade-solutions.github.io'
+  site: 'https://upgrade-solutions.github.io',
+  experimental: { assets: true }
 })
